2505339e4c8b98d8d233a48c844bc871ff5b360e
Parham Alvani <parham.alvani@gmail.com>
2016-03-21 21:52:04 +0430

    Start writing server initiation function.

---
+++ Lamp-REST/src/server.c
@@ -0,0 +1,15 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : server.c
+ *
+ * [] Creation Date : 21-03-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2016 Parham Alvani.
+*/
+
+
---
+++ Lamp-REST/src/server.h
@@ -0,0 +1,19 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : server.h
+ *
+ * [] Creation Date : 21-03-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2016 Parham Alvani.
+*/
+#ifndef SERVER_H
+#define SERVER_H
+
+void server_init();
+
+#endif
