d193f76a81fbfac5585736f428dfa1b22d0bbd68
Parham Alvani <parham.alvani@gmail.com>
2016-03-23 09:45:13 +0430

    Kaa user management function was added.

---
+++ Lamp-REST/src/user.h
@@ -0,0 +1,22 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : user.h
+ *
+ * [] Creation Date : 28-02-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2016 Parham Alvani.
+*/
+
+#ifndef USER_H
+#define USER_H
+
+void attach_endpoint_to_user(kaa_user_manager_t *manager,
+		const char *user_external_id,
+		const char *user_access_token);
+
+#endif
