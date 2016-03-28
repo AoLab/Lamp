bff026995cf9fecbaba4d0c9800e516fad100874
Parham Alvani <parham.alvani@gmail.com>
2016-03-28 08:15:51 +0430

    Add header file for kaa server initiation.

---
+++ Lamp-REST/src/kaa.h
@@ -0,0 +1,20 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : kaa.h
+ *
+ * [] Creation Date : 28-03-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2016 Parham Alvani.
+*/
+
+#ifndef KAA_H
+#define KAA_H
+
+void kaa_init(void);
+
+#endif
