0c450b0abe9a51930317bf2a35510533e85f1e8d
Parham Alvani <parham.alvani@gmail.com>
2016-04-14 12:50:43 +0430

    List REST function template was added.

--- Lamp-REST/src/route.c
+++ Lamp-REST/src/route.c
@@ -70,6 +70,40 @@ static void on_OnI_event_callback(SoupServer *server,
 	soup_message_set_status(msg, SOUP_STATUS_OK);
 }
 
+static void on_List_event_callback(SoupServer *server,
+		SoupMessage *msg,
+		const char *path,
+		GHashTable *query,
+		SoupClientContext *client,
+		gpointer user_data)
+{
+	g_message("Start handling request for %s", path);
+
+	/*
+	 * You must enter the URL currectly we do not
+	 * handle /Lamp/List/foo or ... in this function.
+	 * We response to invalid URLs with NOT FOUND.
+	*/
+	if (g_strcmp0(path, "/Lamp/List")) {
+		soup_message_set_status(msg, SOUP_STATUS_NOT_FOUND);
+		g_message("Invalid path: %s instead of /Lamp/List", path);
+		return;
+	}
+
+	/*
+	 * We have List function with GET method ONLY :)
+	 * so you get NOT IMPLEMENTED error with use other method
+	 * on it.
+	*/
+	if (msg->method != SOUP_METHOD_GET) {
+		soup_message_set_status(msg, SOUP_STATUS_NOT_IMPLEMENTED);
+		g_message("Invalid method: %s instead of GET", msg->method);
+		return;
+	}
+
+	soup_message_set_status(msg, SOUP_STATUS_OK);
+}
+
 static void default_event_callback(SoupServer *server,
 		SoupMessage *msg,
 		const char *path,
@@ -86,6 +120,10 @@ void rest_route_init(SoupServer *server)
 {
 	soup_server_add_handler(server, "/Lamp/OnI",
 			on_OnI_event_callback, NULL, NULL);
+	
+	soup_server_add_handler(server, "/Lamp/List",
+			on_List_event_callback, NULL, NULL);
+
 	soup_server_add_handler(server, NULL,
 			default_event_callback, NULL, NULL);
 }
