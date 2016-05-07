22d8b46bc33c381494cf49e43aa9752958c9c360
Parham Alvani <parham.alvani@gmail.com>
2016-05-07 19:03:21 +0430

    Module init file was added.

---
+++ Lamp-Serial/lamp_serial/__init__.py
@@ -0,0 +1,9 @@
+# In The Name Of God
+# ========================================
+# [] File Name : __init__.py
+#
+# [] Creation Date : 5/7/16
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+__author__ = 'Parham Alvani'
