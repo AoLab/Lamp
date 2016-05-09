a16dadae0d101d7b908a3ad659311ceae701cc86
Parham Alvani <parham.alvani@gmail.com>
2016-05-09 19:38:37 +0400

    Create domain package.

---
+++ Lamp-Serial/lamp_serial/domain/__init__.py
@@ -0,0 +1,9 @@
+# In The Name Of God
+# ========================================
+# [] File Name : __init__.py
+#
+# [] Creation Date : 5/8/16
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+__author__ = 'Parham Alvani'
