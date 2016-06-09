1f2c323a5023da186fb2947afa2dccfbabe80421
Parham Alvani <parham.alvani@gmail.com>
2016-06-09 16:56:55 +0430

    Implement our new log interface :), beta 1

--- Lamp-RPi/src/log.c
+++ Lamp-RPi/src/log.c
@@ -15,11 +15,39 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #include "log.h"
 
-int log_create(struct logger *logger_p, size_t buffer_size, FILE *sink)
+#define LOG_PREFIX_FORMAT "%04d/%02d/%02d %d:%02d:%02d [%s] [%s:%d] (%d) - "
+
+/*
+ * Printable loglevels
+*/
+static const char* log_level_name[] =
 {
+      "NONE"
+    , "FATAL"
+    , "ERROR"
+    , "WARNING"
+    , "INFO"
+    , "DEBUG"
+    , "TRACE"
+};
+
+struct logger *LOGGER;
+
+int log_create(struct logger **logger_p, size_t buffer_size, FILE *sink)
+{
+	*logger_p = malloc(sizeof(struct logger));
+	(*logger_p)->sink = sink;
+	(*logger_p)->buffer_size = buffer_size;
+	(*logger_p)->log_buffer = malloc(sizeof(char) * buffer_size);
+}
+
+int log_global_create(size_t buffer_size, FILE *sink)
+{
+	log_create(&LOGGER, buffer_size, sink);
 }
 
 void sdie(struct logger *self, const char *source_file, int lineno, const char *fmt, ...)
@@ -33,7 +61,6 @@ void sdie(struct logger *self, const char *source_file, int lineno, const char *
 
 	perror(buf);
 	exit(EXIT_FAILURE);
-
 }
 
 void udie(struct logger *self, const char *source_file, int lineno, const char *fmt, ...)
@@ -52,27 +79,38 @@ void udie(struct logger *self, const char *source_file, int lineno, const char *
 
 	fputs(buf, self->sink);
 	exit(EXIT_FAILURE);
-
 }
 
 void ulog(struct logger *self, const char *source_file, int lineno, int log_level, const char *fmt, ...)
 {
-	printf("DEBUG: ");
-	fflush(stdout);
+	size_t len;
 
-	char buf[self->buffer_size];
+	char *path_separator_pos = strrchr(source_file, '/');
+	path_separator_pos = (path_separator_pos ? path_separator_pos : strrchr(source_file, '\\'));
+    	const char *truncated_name = (path_separator_pos ? path_separator_pos + 1 : source_file);
+
+	time_t rawtime;
+	struct tm *timeinfo;
+
+	time(&rawtime);
+	timeinfo = localtime(&rawtime);
+
+	len = sprintf(self->log_buffer, LOG_PREFIX_FORMAT,
+			timeinfo->tm_year, timeinfo->tm_mon, timeinfo->tm_mday,
+			timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec,
+			log_level_name[log_level],
+			truncated_name, lineno, 0);
+
 	va_list args;
 
 	va_start(args, fmt);
-	vsprintf(buf, fmt, args);
+	len += vsprintf(self->log_buffer + len, fmt, args);
 	va_end(args);
 
-	size_t len = strlen(buf);
+	self->log_buffer[len] = '\n';
+	self->log_buffer[len + 1] = 0;
 
-	buf[len] = '\n';
-	buf[len + 1] = 0;
-
-	fputs(buf, self->sink);
+	fputs(self->log_buffer, self->sink);
 }
 
 void slog(struct logger *self, const char *source_file, int lineno, int log_level, const char *fmt, ...)
@@ -88,4 +126,4 @@ void slog(struct logger *self, const char *source_file, int lineno, int log_leve
 	va_end(args);
 
 	perror(buf);
-}	
+}
