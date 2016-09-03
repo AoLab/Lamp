cd94945bddfb593a5b9a9fa323a3e6a44f4a7fb7
Parham Alvani <parham.alvani@gmail.com>
2016-09-03 09:24:49 +0430

    Provide return value for data collecting loop.

--- src/data.h
+++ src/data.h
@@ -15,9 +15,8 @@
 #define DATA_H
 
 #include <kaa/kaa_logging.h>
-#include <kaa/kaa_log.h>
 
 kaa_error_t kaa_data_register(kaa_log_collector_t *log_collector, kaa_logger_t *logger);
-void create_data_collecting_loop(void);
+kaa_error_t create_data_collecting_loop(void);
 
 #endif
