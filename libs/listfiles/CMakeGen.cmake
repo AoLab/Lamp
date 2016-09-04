e4e305933676415ba8c818c6c1a093ebba4cd496
Parham Alvani <parham.alvani@gmail.com>
2016-09-04 23:08:56 +0430

    Libs folder ignored again :|

--- libs/listfiles/CMakeGen.cmake
+++
@@ -1,9 +0,0 @@
-
-
-set (KAA_SOURCE_FILES
-               ${KAA_SOURCE_FILES}
-               src/kaa/gen/kaa_profile_gen.c
-               src/kaa/gen/kaa_logging_gen.c
-               src/kaa/gen/kaa_configuration_gen.c
-               src/kaa/gen/kaa_notification_gen.c
-)
